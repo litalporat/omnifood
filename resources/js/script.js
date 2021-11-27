ac6157b8d8b430bfcdc3c4208a157f757bdbf1f7
Lital Porat <poratlital@gmail.com>
2021-11-27 18:59:30 +0200

    initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    /* demo
+    $('h1').click(function(){
+        $(this).css('background-color', '#ff0000');
+    })*/
+    
+    //sticky navigatoin
+    $('.js--section-features').waypoint(function(direction){
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+
+    
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+        notify(this.element.id + ' hit 25% from top of window') 
+      }, {
+        offset: '25%'
+    })
+
+    //scroll on buttons
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+    });
+    
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+    }); 
+
+    //navigation scroll
+   /* $(function(){
+        $('a[href*=#]:not([href=#])').click(function(){
+            if(location.pathname.replace(/^\//,'')== this.pathname.replace(/^\//,'') && location.hostname== this.hostname){
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if(target.length){
+                    $('html, body').animate({
+                        scrollTop:target.offset().top},1000);
+                        return false;
+                    }
+                }
+            });
+    });*/
+    
+});
